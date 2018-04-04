8d4fe40984cc091fd2bcdcf7a0646f0851121e86
Vu Anh Tu <vuanhtu1993mta@gmail.com>
2018-04-04 13:17:35 +0700

    1. add notes successfully

--- src/app/notes/app.js
+++ src/app/notes/app.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const _ = require('lodash');
 const yargs = require('yargs');
 
-const notes = require('./notes');
+const notes = require('./notes');  // cannot user import * as notes from './notes'; because nodejs not support yet
 
 const argv = yargs.argv;
 let command = process.argv[2];
--- src/app/notes/notes.js
+++ src/app/notes/notes.js
@@ -2,7 +2,15 @@ const fs = require('fs');
 
 let addNote = (title, kind) => {
     console.log("add new notes", title, kind);
-    fs.appendFile('notes.txt', [title, kind]);
+    let note = {
+        title: title,
+        kind: kind
+    };
+    let arrNote = fs.readFileSync('notes.txt');
+    let nodes = JSON.parse(arrNote);
+
+    nodes.push(note);
+    fs.writeFileSync('notes.txt', JSON.stringify(nodes));
 };
 let listNote = () => {
     console.log("Get list notes");
