33e29b7ebeda931a5e79452bd62b9f2525b38ea3
Vu Anh Tu <vuanhtu1993mta@gmail.com>
2018-04-04 13:23:01 +0700

    1. Initial learn machine app

--- src/app/greeting/france.js
+++
@@ -1,7 +0,0 @@
-var data = require('./greeting.json');
-
-var greeting = function () {
-  console.log(data.fran);
-};
-
-module.exports = greeting;
--- src/app/require-moduleExport/require.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let file = require('./export'); // file in here to storage module.exports of exports file
-console.log('require file here');
-console.log(file);
-// fs.appendFile('  fsFile.txt', `storage sth in here ${file.person}`);
