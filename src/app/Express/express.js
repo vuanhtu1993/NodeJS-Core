a890f79c1ec3e1e8b0c7403fd236105c7bedb7da
Vu Anh Tu <vuanhtu1993mta@gmail.com>
2018-04-06 10:05:01 +0700

    1. understanding param and query at url

--- src/app/Express/express.js
+++ src/app/Express/express.js
@@ -3,9 +3,8 @@ const app = express();
 const port = 3000;
 
 app.use('/assets', express.static('../../public'));
-app.get('/', (request, response) => {
-    console.log(__dirname + '../../public');
-    response.send(`
+app.get('/', (req, res) => {
+    res.send(`
    <html>
     <head>
         <link rel="stylesheet" href="assets/style.css">
@@ -16,8 +15,23 @@ app.get('/', (request, response) => {
    <html>
    `);
 });
-app.get('/api', (request, response) => {
-    response.json({name: "Anh Tus", age: "26", carrier: "Web developer"});
+app.get('/api', (req, res) => {
+    res.json({name: "Anh Tus", age: "26", carrier: "Web developer"});
+});
+app.get('/person/:id', (req, res) => {
+    console.log(req.query);
+    res.send(`
+      <html>
+    <head>
+        <link rel="stylesheet" href="/assets/style.css">
+    </head> 
+    <body>
+        <h4> Person: ${req.params.id}</h4> 
+        <h4> Query string: ${req.query.abc}</h4> 
+    </body>
+   <html>
+        
+    `)
 });
 app.listen(port, () => {
     console.log(`Server running on port: ${port}`);
