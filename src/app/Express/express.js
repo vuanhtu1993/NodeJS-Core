0d7c9c99cb9af7f77e1b09e691c20b8507670420
Vu Anh Tu <vuanhtu1993mta@gmail.com>
2018-04-09 13:45:53 +0700

    1. install body-parser 2. using body-parser to get req.body

--- src/app/Express/express.js
+++ src/app/Express/express.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
-const port = 3000;
 
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+const urlencodeParser = bodyParser.urlencoded({ extended: false });
+
+const port = 3000;
+// routing
 app.use('/assets', express.static('../../public'));
 app.get('/', (req, res) => {
     res.send(`
@@ -11,6 +16,12 @@ app.get('/', (req, res) => {
     </head> 
     <body>
         <h1>Hello world</h1>   
+        <h4>url encode</h4>
+        <form action="/login" method="post">
+            user: <input type="text" id="user" name="user"><br>
+            password: <input type="text" id="password" name="password">
+            <button type="submit">submit</button>
+        </form>
     </body>
    <html>
    `);
@@ -27,12 +38,23 @@ app.get('/person/:id', (req, res) => {
     </head> 
     <body>
         <h4> Person: ${req.params.id}</h4> 
-        <h4> Query string: ${req.query.abc}</h4> 
+        <h4> Query string: ${req.query}</h4> 
     </body>
    <html>
         
     `)
 });
+// body parser
+app.post('/login', urlencodeParser, (req, res) => {
+    if (!req.body) return res.sendStatus(400);
+    res.send(`${req.body.user} access url login`);
+});
+
+app.post('/person', jsonParser, (req, res) => {
+    if (!req.body) return res.sendStatus(400)
+    // create user in req.body
+});
+
 app.listen(port, () => {
     console.log(`Server running on port: ${port}`);
 });
