187ba265f44aafa84794f70605e85034b5daaabb
Vu Anh Tu <vuanhtu1993mta@gmail.com>
2018-04-21 13:38:04 +0700

    1. finished update function

--- src/app/mongoDB/mongoose.js
+++ src/app/mongoDB/mongoose.js
@@ -1,30 +1,64 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-const port = 3400;
+const port = 6789;
+
+// bodyParser middleware
+// create application/json parser
+// let jsonParser = bodyParser.json();
+app.use(bodyParser.json());
+
+// create application/x-www-form-urlencoded parser
+let urlencodedParser = bodyParser.urlencoded({ extended: false });
 const mongoose = require('mongoose');
+mongoose.connect("mongodb://test:test@ds151169.mlab.com:51169/fresher");
 
-mongoose.connect("mongodb://test:test@cluster0-shard-00-00-bwpse.mongodb.net:27017,cluster0-shard-00-01-bwpse.mongodb.net:27017,cluster0-shard-00-02-bwpse.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin");
-
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', function () {
+	console.log('Connect Mlab DB successfully');
+});
+// Init schema
 let Schema = mongoose.Schema;
+
 let personScheme = new Schema({
-    firstName: String,
-    lastName: String,
-    address: String,
+	firstName: String,
+	lastName: String,
+	address: String,
 });
+
 let Person = mongoose.model("Person", personScheme);
-let newPerson = Person({firstName:"Anh Tus", lastName:"Vu", address:"Hanoi"});
-newPerson.save(function (err) {
-    if (err) throw err;
-    console.log('New person saved !')
+
+// API
+app.get('/users', function (req, res) {
+	Person.find()
+		.then((data) => res.send(data))
+		.catch((err) => res.send(err))
 });
-app.get('/',function (req, res) {
-    res.send('welcome to mongodb server');
-    Person.find({}, function (err, users) {
-        if (err) throw err;
-        console.log(users);
-    })
+
+app.put('/user/:id', function (req, res) {
+	let _id = {"_id": req.params.id};
+	console.log('body', req.body)
+	const { firstName, lastName, address } = req.body
+	Person.findOneAndUpdate({_id},
+		{
+			firstName,
+			lastName,
+			address
+		}
+		)
+		.then((data) => res.send(data))
+		.catch((err) => console.log(err))
 });
+
+app.post('/users', function (req, res) {
+	let newPerson = new Person(req.body);
+	newPerson.save()
+		.then((data) => res.send(data))
+		.catch((err) => res.send(err))
+});
+
 
 app.listen(port, function () {
-    console.log('sql server running')
+	console.log('Server using MongoDB is running!')
 });
