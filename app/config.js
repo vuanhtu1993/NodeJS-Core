d83a8563194d3c99097c6db7b80d075f70d4bc3d
Vu Anh Tu <vuanhtu1993mta@gmail.com>
2018-04-30 01:19:40 +0700

    1. install dependency 2. create User table 3. Create config DB file

---
+++ app/config.js
@@ -0,0 +1,6 @@
+const config = {
+	"secret": "vuanhtu1993",
+	"database": "mongodb://admin:admin@ds263619.mlab.com:63619/medium"
+};
+
+export default config;
---
+++ app/model/user.js
@@ -0,0 +1,15 @@
+// get instance of mongoose
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create User Schema
+const User = new Schema({
+	email: String,
+	token: String,
+	username: String,
+	bio: String,
+	image: String
+});
+
+// Set up mongoose model
+const userTable = mongoose.model('User', User);
