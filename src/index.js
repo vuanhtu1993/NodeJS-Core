4090fdbdfc325ba656dc4261269a690d088bdc9f
Vu Anh Tu <vuanhtu1993mta@gmail.com>
2018-05-07 13:37:02 +0700

    1. Build server 2. Create User schema

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
 const port = process.env.port || 8000;
 const app = express();
+const url = 'mongodb://test:test@ds117070.mlab.com:17070/testmock';
 
 app.get('/', (req, res) => {
 	res.send('xxxx');
 });
-
+mongoose.connect(url)
+	.then(() => console.log('MongoDB is running!'))
+	.catch((err) => console.log(err));
 app.listen(port, () => {
 	console.log('server is running on '+ port)
 });
