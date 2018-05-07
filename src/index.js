80ef510b722140a125509bff688709831dc0dbed
Vu Anh Tu <vuanhtu1993mta@gmail.com>
2018-05-07 22:46:21 +0700

    Additional body-parser middleware

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+import router from './controllers/api';
 const port = process.env.port || 8000;
 const app = express();
 const url = 'mongodb://test:test@ds117070.mlab.com:17070/testmock';
 
-app.get('/', (req, res) => {
-	res.send('xxxx');
-});
+//setting body-parser
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// API
+app.use('/', router);
+
+// Connect MongoDB
 mongoose.connect(url)
 	.then(() => console.log('MongoDB is running!'))
 	.catch((err) => console.log(err));
