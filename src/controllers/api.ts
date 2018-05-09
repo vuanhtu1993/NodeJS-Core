ae80f5df6186cf5b7897bc8da37d48683e1d2b04
Vu Anh Tu <vuanhtu1993mta@gmail.com>
2018-05-09 15:40:39 +0700

    implicitAny: true yarn tslint src/**/*.ts --fix

--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -1,23 +1,26 @@
-import * as express from 'express';
-const router = express.Router();
-import User from '../models/User';
+import * as express from "express";
+// import {Request, Response, NextFunction} from "express";
 
-router.post('/api/users', (req, res, next) => {
-	const { username, email, password } = req.body.user;
-	let user = new User();
-	user.username = username;
-	user.password = user.setPassword(password);
-	user.email = email;
-	user.save()
-		.then((user) => res.status(200).json({
-			success: true,
-			user: user,
-		}))
-		.catch((err) => res.status(400).json({
-			success: false,
-			error: err,
-		}))
+const router = express.Router();
+import User from "../models/User";
+
+
+router.post('/api/users', (req: any, res: any, next: any) => {
+    const {username, email, password} = req.body.user;
+    const user = new User() as any;
+    user.username = username;
+    user.password = user.setPassword(password);
+    user.email = email;
+    user.save()
+        .then((user: any) => res.status(200).json({
+            success: true,
+            user,
+        }))
+        .catch((err: any) => res.status(400).json({
+            success: false,
+            error: err,
+        }));
 });
 
-router.get('/api/abc', (req, res) => res.send('xxx'));
+router.get("/api/abc", (req, res) => res.send("xxx"));
 export default router;
