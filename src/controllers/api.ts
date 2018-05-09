17c0cdb5a4953d0e78e9667b64184920ebcef6d7
Vu Anh Tu <vuanhtu1993mta@gmail.com>
2018-05-09 16:24:01 +0700

    files after tslint airbnb

--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -1,26 +1,26 @@
-import * as express from "express";
+import * as express from 'express';
 // import {Request, Response, NextFunction} from "express";
 
 const router = express.Router();
-import User from "../models/User";
+import User from '../models/User';
 
 
 router.post('/api/users', (req: any, res: any, next: any) => {
-    const {username, email, password} = req.body.user;
-    const user = new User() as any;
-    user.username = username;
-    user.password = user.setPassword(password);
-    user.email = email;
-    user.save()
-        .then((user: any) => res.status(200).json({
-            success: true,
-            user,
-        }))
-        .catch((err: any) => res.status(400).json({
-            success: false,
-            error: err,
-        }));
+  const { username, email, password } = req.body.user;
+  const user = new User() as any;
+  user.username = username;
+  user.password = user.setPassword(password);
+  user.email = email;
+  user.save()
+    .then((user: any) => res.status(200).json({
+      success: true,
+      user,
+    }))
+    .catch((err: any) => res.status(400).json({
+      success: false,
+      error: err,
+    }));
 });
 
-router.get("/api/abc", (req, res) => res.send("xxx"));
+router.get('/api/abc', (req, res) => res.send('xxx'));
 export default router;
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,41 +1,41 @@
-import * as md5 from "md5";
-import * as mongoose from "mongoose";
-import * as  uniqueValidator from "mongoose-unique-validator";
+import * as md5 from 'md5';
+import * as mongoose from 'mongoose';
+import * as  uniqueValidator from 'mongoose-unique-validator';
 
 const UserSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        lowercase: true,
-        unique: true,
-        required: [true, "can't be blank"],
-        match: [/^[a-zA-Z0-9]+$/, "is invalid"],
-        index: true,
-    },
-    email: {
-        type: String,
-        lowercase: true,
-        unique: true,
-        required: [true, "can't be blank"],
-        match: [/\S+@\S+\.\S+/, "is invalid"],
-        index: true,
-    },
-    password: String,
-    bio: String,
-    image: String,
-    favorites: [{type: mongoose.Schema.Types.ObjectId, ref: "Article"}],
-    following: [{type: mongoose.Schema.Types.ObjectId, ref: "User"}],
-    hash: String,
-    salt: String,
-}, {timestamps: true});
+  username: {
+    type: String,
+    lowercase: true,
+    unique: true,
+    required: [true, 'can\'t be blank'],
+    match: [/^[a-zA-Z0-9]+$/, 'is invalid'],
+    index: true,
+  },
+  email: {
+    type: String,
+    lowercase: true,
+    unique: true,
+    required: [true, 'can\'t be blank'],
+    match: [/\S+@\S+\.\S+/, 'is invalid'],
+    index: true,
+  },
+  password: String,
+  bio: String,
+  image: String,
+  favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Article' }],
+  following: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  hash: String,
+  salt: String,
+},                                     { timestamps: true });
 
-UserSchema.plugin(uniqueValidator, {message: "is already taken!"});
+UserSchema.plugin(uniqueValidator, { message: 'is already taken!' });
 
 // User feature
-UserSchema.methods.setPassword = function(pwd: string) {
-    return md5(pwd);
+UserSchema.methods.setPassword = function (pwd: string) {
+  return md5(pwd);
 };
 // Kết nối với bảng User trong DB
-mongoose.model("User", UserSchema);
+mongoose.model('User', UserSchema);
 // Export Schema User for controller
-const User = mongoose.model("User");
+const User = mongoose.model('User');
 export default User;
