dab26799be5e42b2e476425ae318bdd8576959ea
vuanhtu1993mta@gmail.com <tuntun159753>
2018-05-31 11:19:39 +0700

    config express session and passport along ordering configuration

--- app/server.js
+++ app/server.js
@@ -1,31 +1,49 @@
 import express from 'express';
+
 const app = express();
 
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
 import mongoose from 'mongoose';
 
-
 import config from './config';
 import router from "./router/index";
 
 const port = process.env.PORT || 8080;
 mongoose.connect(config.database, (err) => {
-	if (err) {
-		console.log('Error initializing connection to MongoDB');
-		return;
-	} else {
-		console.log('Connect to mongoDB successfully');
-	}
+  if (err) {
+    console.log('Error initializing connection to MongoDB');
+    return;
+  } else {
+    console.log('Connect to mongoDB successfully');
+  }
 });
-app.set('superSecret', config.secret); // secret variable
 
 // use body parser so we can get info from POST and/or URL parameters
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 // use morgan to log requests to the console
 app.use(morgan('dev'));
+
+// express-session
+import session from 'express-session';
+app.use(session({
+  secret: 'secret',
+  saveUninitialized: 'true',
+  resave: true,
+}));
+
+// =======================
+// passport ==============
+// =======================
+import passport from 'passport';
+import passportLocal from 'passport-local';
+
+const LocalStrategy = passportLocal.Strategy;
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 // =======================
 // routes ================
